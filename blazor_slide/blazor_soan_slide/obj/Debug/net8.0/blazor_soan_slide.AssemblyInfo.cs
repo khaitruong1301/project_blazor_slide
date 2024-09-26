21c883881517b84d92ef8808f0eab1292b808324
KaitoKido <khaitruong1301@gmail.com>
2024-09-27 05:22:13 +0700

    dp

--- blazor_slide/blazor_soan_slide/obj/Debug/net8.0/blazor_soan_slide.AssemblyInfo.cs
+++ blazor_slide/blazor_soan_slide/obj/Debug/net8.0/blazor_soan_slide.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor_soan_slide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e3cc8846b418d95b9a2a6990b203e8aec8f19b")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor_soan_slide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor_soan_slide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
