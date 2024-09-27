29848c15d11b009dfad016e4a86ece444e985678
khai <khaitruong1301@gmail.com>
2024-09-28 01:54:12 +0700

    dp

---
+++ blazor_slide/blazor_soan_slide/Models/UserRegister.cs
@@ -0,0 +1,9 @@
+namespace blazor_soan_slide.Models;
+class UserRegister {
+    public string userName {get;set;}
+    public string password {get;set;}
+    public string fullname {get;set;}
+    public string gender {get;set;}
+    public string phone {get;set;}
+    public string country {get;set;}
+}
--- blazor_slide/blazor_soan_slide/obj/Debug/net8.0/blazor_soan_slide.AssemblyInfo.cs
+++ blazor_slide/blazor_soan_slide/obj/Debug/net8.0/blazor_soan_slide.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor_soan_slide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e3cc8846b418d95b9a2a6990b203e8aec8f19b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21c883881517b84d92ef8808f0eab1292b808324")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor_soan_slide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor_soan_slide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
